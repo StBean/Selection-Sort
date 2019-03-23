4ffe0368f8dd6714b49f639b1875ad0161e3d9a2
Anthony Poblacion <StBean@users.noreply.github.com>
2019-03-23 02:35:42 -0700

    Create selectSort.js

---
+++ selectSort.js
@@ -0,0 +1,14 @@
+function selectSort(data){
+	for(var i = 0; i < data.length - 1; i++){
+		var min = i; 
+		for(var j = i + 1; j < data.length; j++){
+			if(data[j] < data[min]){
+				min = j;
+			}
+			var temp = data[i];
+			data[i] = data[min];
+			data[min] = temp;
+		}
+	}
+	return data;
+};
